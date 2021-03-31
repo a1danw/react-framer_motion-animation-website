4bc4255a828a5861fb1cf89205b1f8ac6ca38d36
Aidan <walkeraidan94@gmail.com>
2021-03-31 14:17:30 +0100

    Updates

---
+++ precache-manifest.7d27aeda8e208037765c910d451b1479.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09967397258a580d7f49ce80ebb9abc8",
+    "url": "/react-framer_motion-animation-website/index.html"
+  },
+  {
+    "revision": "b28cf7f90a2e69712f00",
+    "url": "/react-framer_motion-animation-website/static/js/2.b17fa40e.chunk.js"
+  },
+  {
+    "revision": "aa4100970f46d7e0ec2af84de3f2740b",
+    "url": "/react-framer_motion-animation-website/static/js/2.b17fa40e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7cedf14a968c2c6a7651",
+    "url": "/react-framer_motion-animation-website/static/js/main.fe75a575.chunk.js"
+  },
+  {
+    "revision": "86af1cabfb65c3257066",
+    "url": "/react-framer_motion-animation-website/static/js/runtime-main.bef29768.js"
+  },
+  {
+    "revision": "f85903d473cb19505e2aa6c63a0db00b",
+    "url": "/react-framer_motion-animation-website/static/media/athlete-small.f85903d4.png"
+  },
+  {
+    "revision": "6d53898d42640dad92347101b160da7f",
+    "url": "/react-framer_motion-animation-website/static/media/athlete2.6d53898d.png"
+  },
+  {
+    "revision": "f5c64a5f820c4ac31423ed0ee5d95c7c",
+    "url": "/react-framer_motion-animation-website/static/media/clock.f5c64a5f.svg"
+  },
+  {
+    "revision": "f620483184a3efa896127ee80cab531c",
+    "url": "/react-framer_motion-animation-website/static/media/diaphragm.f6204831.svg"
+  },
+  {
+    "revision": "e149f59c6323b18f98e987cd564e1fc9",
+    "url": "/react-framer_motion-animation-website/static/media/good-times2.e149f59c.jpg"
+  },
+  {
+    "revision": "169a9b3ae8eff8e916dc7711c7546c64",
+    "url": "/react-framer_motion-animation-website/static/media/goodtimes-small.169a9b3a.png"
+  },
+  {
+    "revision": "99a18d77af2a0e70784a842789ab9a34",
+    "url": "/react-framer_motion-animation-website/static/media/home1.99a18d77.png"
+  },
+  {
+    "revision": "ee6a60799cceb9c60ce67b133ca6574c",
+    "url": "/react-framer_motion-animation-website/static/media/home2.ee6a6079.png"
+  },
+  {
+    "revision": "d1749735b44aafa17334e7710263aac3",
+    "url": "/react-framer_motion-animation-website/static/media/money.d1749735.svg"
+  },
+  {
+    "revision": "8c795ad57e55b06d76b8a73b27a13fd1",
+    "url": "/react-framer_motion-animation-website/static/media/teamwork.8c795ad5.svg"
+  },
+  {
+    "revision": "74873026dbed7229712447d165a7a2fd",
+    "url": "/react-framer_motion-animation-website/static/media/the-racer2.74873026.jpg"
+  },
+  {
+    "revision": "ca518f44c6e1ddc450654002971bb467",
+    "url": "/react-framer_motion-animation-website/static/media/theracer-small.ca518f44.png"
+  }
+]);
